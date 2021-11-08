9bdf2bca33b970ec5abbca7835e7ebf82f791f4e
GitTest <https://github.com/Mauri2027/GitTest.git>
2021-11-08 20:54:41 -0300

    spring boot mvc funcionando con tomcat, incopora html y css

---
+++ src/main/java/com/examplespring/pruebamvc/PruebamvcApplication.java
@@ -0,0 +1,13 @@
+package com.examplespring.pruebamvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebamvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebamvcApplication.class, args);
+	}
+
+}
